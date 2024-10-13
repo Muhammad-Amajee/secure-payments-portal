77868e781fa0800c03c0f12e4f2c3a1afec33d3c
Segadimane-Makau <104513117+Muhammad-Amajee@users.noreply.github.com>
2024-10-13 12:50:08 +0200

    Update validationMessages.js

--- client/src/validationMessages.js
+++ client/src/validationMessages.js
@@ -1,5 +1,5 @@
 // client/src/validationMessages.js
 export const validationMessages = {
   username: 'Username must be 5-20 characters long and alphanumeric with underscores allowed.',
-  pwdValidation: 'Password must be 8-20 characters long and can include special characters.',
+  pwdValidation: 'Your passphrase must be 8-20 characters long and can include special characters.',
 };
