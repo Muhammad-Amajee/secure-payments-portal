44b0cfc02b7bbf5e8eca1cddffee17c408650007
Segadimane-Makau <104513117+Muhammad-Amajee@users.noreply.github.com>
2024-10-13 12:44:59 +0200

    fixing sonarcloud security error

--- client/src/validationMessages.js
+++ client/src/validationMessages.js
@@ -1,5 +1,5 @@
 // client/src/validationMessages.js
 export const validationMessages = {
   username: 'Username must be 5-20 characters long and alphanumeric with underscores allowed.',
-  passwordValidation: 'Password must be 8-20 characters long and can include special characters.',
+  pwdValidation: 'Password must be 8-20 characters long and can include special characters.',
 };
