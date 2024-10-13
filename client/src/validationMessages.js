c3a3530c2d32681ab78e23192eb9c2c2d724fcb5
Segadimane-Makau <104513117+Muhammad-Amajee@users.noreply.github.com>
2024-10-13 12:30:43 +0200

    fixing the sonarQube security hotspot

--- client/src/validationMessages.js
+++ client/src/validationMessages.js
@@ -1,5 +1,5 @@
 // client/src/validationMessages.js
 export const validationMessages = {
     username: 'Username must be 5-20 characters long and alphanumeric with underscores allowed.',
-    password: 'Password must be 8-20 characters long and can include special characters.',
+    passwordString: 'Password must be 8-20 characters long and can include special characters.',
   };
